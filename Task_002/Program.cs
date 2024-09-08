533c23a6a71899e729dcf3594af35c36321e14ba
liubov <Madamlub98@yandex.ru>
2024-09-08 11:41:47 +0300

    Добавили решение задачи 2

---
+++ Task_002/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Задайте строку, содержащую латинские буквы 
+// в обоих регистрах. Сформируйте строку, в которой все 
+// заглавные буквы заменены на строчные.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Шаг 1: Задаем строку
+        string inputString = "Hello World! This Is A Test String.";
+
+        // Шаг 2: Преобразуем строку в нижний регистр
+        string lowerCaseString = inputString.ToLower();
+
+        // Шаг 3: Выводим результат
+        Console.WriteLine("Исходная строка: " + inputString);
+        Console.WriteLine("Строка в нижнем регистре: " + lowerCaseString);
+    }
+}
